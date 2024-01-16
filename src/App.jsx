f156c146c38f0ed2c7d207784fe168a3bd10d866
Austin <guitarguy199@gmail.com>
2024-01-15 17:05:18 -0700

    typo fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
 import Home from './Pages/Home'
+import About from './Pages/About/index.jsx'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
      <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
      </Layout>
     </>
